d34f93520dfb4367818eae67e8193c2716533bd7
Karnatapu-Manoj <karnatapumanoj@gmail.com>
2022-04-07 11:01:47 +0530

    Add project files.

---
+++ Movies_E_Commerce_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Movies_E_Commerce_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
